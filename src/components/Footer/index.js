efbb988edd4ac59f489651768c4477a77aaf3857
Fabio Ernanni <ernannicf@gmail.com>
2023-10-24 01:04:57 -0300

    added footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,10 +3,15 @@ import "./index.css";
 const Footer = () => {
   return (
     <footer>
-      <img
-        src="/images/fundo.png"
-        alt="Rodapé"
-      />
+      <div className="socialLinks">
+        <a target="_blank" href="https://www.facebook.com/ernanni" rel="noreferrer"><img src="/images/fb.png" alt="facebook" /></a>
+        <a target="_blank" href="https://www.twitter.com/ernannitxt" rel="noreferrer"><img src="/images/tw.png" alt="twitter" /></a>
+        <a target="_blank" href="https://www.instagram.com/ernanni.jpg" rel="noreferrer"><img src="/images/ig.png" alt="instagram" /></a>
+      </div>
+      <div>
+        <img src="/images/logo.png" alt="logo organo" />
+      </div>
+      <div><a target="_blank" href="https://github.com/ernanni" rel="noreferrer">Desenvolvidor por Fabio Ernanni</a></div>
     </footer>
   );
 };
